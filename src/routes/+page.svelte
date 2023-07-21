ebbb7bd2e79d63a752dbd4faee7b6656c153a8a5
Adam Simon <adamsimon488@gmail.com>
2023-07-21 15:36:02 +0200

    Added authentication on all protected routes and, redireting from main page if user is signed in

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,17 @@
 <script lang="ts">
   import GetStarted from './GetStarted.svelte';
   import Preview from './Preview.svelte';
+  import { onMount } from 'svelte';
+  import { goto } from '$app/navigation';
+
+  onMount(async () => {
+    const response = await fetch('/api', { method: 'GET' });
+    const user = await response.json();
+    console.log(user);
+    if (user.status == 200) {
+      goto('/home');
+    }
+  });
 </script>
 
 <main class="bg-gray-900 h-auto flex justify-center items-center flex-col">

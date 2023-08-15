7792b52151f1235f93b60394c9a6ab0c819df20e
Adam Simon <adamsimon488@gmail.com>
2023-08-15 11:15:39 +0200

    Added a CSP

--- svelte.config.js
+++ svelte.config.js
@@ -14,6 +14,25 @@ const config = {
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter(),
     files: {},
+    csp: {
+      reportOnly: {
+        'default-src': ['self', 'https://fonts.googleapis.com', 'https://fonts.gstatic.com'],
+        'script-src': ['self'],
+        'style-src': ['self', 'https://fonts.googleapis.com', 'https://fonts.gstatic.com'],
+        'img-src': ['self'],
+        'connect-src': ['self'],
+        'object-src': ['self'],
+        'media-src': ['self'],
+        'frame-src': ['self'],
+        'child-src': ['self'],
+        'form-action': ['self'],
+        'frame-ancestors': ['none'],
+        'base-uri': ['self'],
+        'worker-src': ['none'],
+        'manifest-src': ['none'],
+        'report-uri': ['/report'],
+      }
+    },
 
     alias: {
       $lib: path.resolve('./src/lib')
